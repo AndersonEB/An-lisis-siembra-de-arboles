06a422b087489fb38f81941d8e63fe7c567a09fb
wilmar castañeda <wilmarcj@gmail.com>
2023-02-22 10:51:44 -0500

    se crea el filtro 1

---
+++ filtro1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+bd = pd.read_csv('./Siembras.csv')
+
+columns = bd.columns.values
+print(columns)
+
+print('\n')
+print('FILTRO1: Encontrar todos los datos de santa fe de Antioquia donde se tengan siembras de + de 250 arboles')
+
+mas250arboles = bd[bd["Arboles"]>250]
+print(mas250arboles)
+
+
+
+archivoHTML = mas250arboles.to_html()
+with open("./tabla.html", 'wb') as f:
+    f.write(bytes(archivoHTML, encoding= 'utf-8'))
